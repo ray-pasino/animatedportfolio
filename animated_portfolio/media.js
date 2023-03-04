3c4994c8d86bcaa8a8ae9947b8ba0e08bfc05460
ray-pasino <115047105+ray-pasino@users.noreply.github.com>
2023-03-04 07:55:08 -0800

    Add files via upload

---
+++ animated_portfolio/media.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer", "Youtuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+})
+
+
